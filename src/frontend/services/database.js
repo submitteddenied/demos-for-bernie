ecf1fca8f544b185cfb48579f94e2930c2cea1ee
Michael Jensen <emjay1988@gmail.com>
2020-03-05 23:38:10 -0800

    Read display name from auth

--- src/frontend/services/database.js
+++ src/frontend/services/database.js
@@ -1,16 +1,25 @@
 import firebase from './firebase'
 const db = firebase.firestore()
 
+const getDisplayName = () => {
+  const user = firebase.auth().currentUser
+  if(user) {
+    return user.displayName
+  }
+
+  return window.location.search.slice(1)
+}
 
 export default {
   getCount: () => {
-    return db.doc('demos/submitteddenied').get()
+    return db.doc('demos/' + getDisplayName()).get()
       .then((doc) => {
         return {count: doc.get('demos')}
       })
   },
   incrementCount: (count) => {
-    const doc = db.doc('demos/submitteddenied')
+    const user = firebase.auth().currentUser
+    const doc = db.doc('demos/' + getDisplayName())
     doc.update('demos', firebase.firestore.FieldValue.increment(count))
       .then(() => {
         return doc.get()
